66ad0ba658e4076b6707ec91c405ca8d95743bfb
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2024-10-14 10:15:35 +0100

    Abstract out common code.

--- impossibleStairs.py
+++ impossibleStairs.py
@@ -1,42 +1,30 @@
 import maya.cmds as mc
 
-def main():
+def generate_stairs():
     mc.select(all=True)
-    mc.delete()  # Clean the scene
+    mc.delete()  # Clear the scene
 
     num_steps = 0
-    x_coord = -6
-    z_coord = 6
     cube_height = 20
+    coord_dict = {'x': -6, 'z': 6}
 
-    while num_steps < 7:
-        z_coord -= 2
-        cube_height = create_cube(cube_height, x_coord, z_coord)
-        num_steps += 1
+    cube_height = generate_flight_of_stairs(coord_dict, 0, -2, cube_height, 7)
+    cube_height = generate_flight_of_stairs(coord_dict, 2, 0, cube_height, 5)
+    cube_height = generate_flight_of_stairs(coord_dict, 0, 2, cube_height, 4)
+    cube_height = generate_flight_of_stairs(coord_dict, -2, 0, cube_height, 2)
 
+def generate_flight_of_stairs(p_coord_dict, p_x_change, p_z_change, p_cube_height, p_num_steps_in_flight):
     num_steps = 0
-    while num_steps < 5:
-        x_coord += 2
-        cube_height = create_cube(cube_height, x_coord, z_coord)
+    while num_steps < p_num_steps_in_flight:
+        p_coord_dict['x'] += p_x_change
+        p_coord_dict['z'] += p_z_change
+        p_cube_height = create_cube(p_cube_height, p_coord_dict['x'], p_coord_dict['z'])
         num_steps += 1
 
-    num_steps = 0
-    while num_steps < 4:
-        z_coord += 2
-        cube_height = create_cube(cube_height, x_coord, z_coord)
-        num_steps += 1
-
-    num_steps = 0
-
-    while num_steps < 2:
-        x_coord -= 2
-        cube_height = create_cube(cube_height, x_coord, z_coord)
-        num_steps += 1
-
+    return p_cube_height
 
 def create_cube(p_cube_height, p_x_coord, p_z_coord):
-    # Create cube
-    curr_cube = mc.polyCube(d=2, h=p_cube_height, w=2)[0]  # Get the cube object
+    curr_cube = mc.polyCube(d = 2, h = p_cube_height, w = 2)[0]
 
     # Ensure that the newly created cube is the active selection
     mc.select(curr_cube)
@@ -49,9 +37,8 @@ def create_cube(p_cube_height, p_x_coord, p_z_coord):
 
     return (p_cube_height + 0.25)
 
-main()
+generate_stairs()
 
 # TODO: polyExtrude front face of first cube
 # TODO: add light source
 # TODO: add perspective camera
-# TODO: abstract out common code
