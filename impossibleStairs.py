38c883deaf283847c131ed5f57479071c65d2fe4
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2024-10-16 12:03:11 +0100

    Refactor steps code; Renaming from cube to step.

--- impossibleStairs.py
+++ impossibleStairs.py
@@ -1,7 +1,7 @@
 import maya.cmds as mc
 import random
 
-CUBE_HEIGHT_WIDTH = 2
+SQ_STEP_SIZE = 2
 
 def clear_scene():
     mc.select(all=True)
@@ -9,43 +9,43 @@ def clear_scene():
 
 def generate_stairs():
     num_steps = 0
-    cube_height = 20
+    step_height = 20
     coord_dict = {'x': -6, 'z': 4} # Values arbitrarily decided after manual testing
 
     # Generate the first step and extrude its front face
-    cube_height, first_cube = create_cube(cube_height, coord_dict)
+    step_height, first_step = create_step(step_height, coord_dict)
 
-    # Polyextrude the front face of the first cube
-    mc.polyExtrudeFacet(first_cube + ".f[0]", ltz=0.4)  # ltz determined after manual testing
+    # Polyextrude the front face of the first step
+    mc.polyExtrudeFacet(first_step + ".f[0]", ltz=0.4)  # ltz determined after manual testing
 
-    cube_height = generate_flight_of_stairs(coord_dict, 0,                  -CUBE_HEIGHT_WIDTH, cube_height, 6)
-    cube_height = generate_flight_of_stairs(coord_dict, CUBE_HEIGHT_WIDTH,  0,                  cube_height, 5)
-    cube_height = generate_flight_of_stairs(coord_dict, 0,                  CUBE_HEIGHT_WIDTH,  cube_height, 4)
-    cube_height = generate_flight_of_stairs(coord_dict, -CUBE_HEIGHT_WIDTH, 0,                  cube_height, 2)
+    step_height = generate_flight_of_stairs(coord_dict, 0,             -SQ_STEP_SIZE, step_height, 6)
+    step_height = generate_flight_of_stairs(coord_dict, SQ_STEP_SIZE,  0,             step_height, 5)
+    step_height = generate_flight_of_stairs(coord_dict, 0,             SQ_STEP_SIZE,  step_height, 4)
+    step_height = generate_flight_of_stairs(coord_dict, -SQ_STEP_SIZE, 0,             step_height, 2)
 
-def generate_flight_of_stairs(p_coord_dict, p_x_delta, p_z_delta, p_cube_height, p_num_steps_in_flight):
+def generate_flight_of_stairs(p_coord_dict, p_x_delta, p_z_delta, p_step_height, p_num_steps_in_flight):
     num_steps = 0
     while num_steps < p_num_steps_in_flight:
         p_coord_dict['x'] += p_x_delta
         p_coord_dict['z'] += p_z_delta
-        p_cube_height = create_cube(p_cube_height, p_coord_dict)[0]
+        p_step_height = create_step(p_step_height, p_coord_dict)[0]
         num_steps += 1
 
-    return p_cube_height
+    return p_step_height
 
-def create_cube(p_cube_height, p_coord_dict):
-    curr_cube = mc.polyCube(d = CUBE_HEIGHT_WIDTH, h = p_cube_height, w = CUBE_HEIGHT_WIDTH)[0]
+def create_step(p_step_height, p_coord_dict):
+    curr_step = mc.polyCube(d = SQ_STEP_SIZE, h = p_step_height, w = SQ_STEP_SIZE)[0]
 
-    # Ensure that the newly created cube is the active selection
-    mc.select(curr_cube)
+    # Ensure that the newly created step is the active selection
+    mc.select(curr_step)
     
-    # Move pivot to the bottom of the cube (Y = -p_cube_height / 2)
-    mc.xform(curr_cube, pivots=(0, (-p_cube_height / 2), 0), ws=True)
+    # Move pivot to the bottom of the step (Y = -p_step_height / 2)
+    mc.xform(curr_step, pivots=(0, (-p_step_height / 2), 0), ws=True)
     
-    # Move the cube so its base aligns with Y = 0 and position it along the X-axis
-    mc.move(p_coord_dict['x'], (p_cube_height / 2), p_coord_dict['z'], curr_cube, ws=True)  # y = (p_cube_height / 2) keeps the base at Y=0
+    # Move the step so its base aligns with Y = 0 and position it along the X-axis
+    mc.move(p_coord_dict['x'], (p_step_height / 2), p_coord_dict['z'], curr_step, ws=True)  # y = (p_step_height / 2) keeps the base at Y=0
 
-    return ((p_cube_height + 0.25), curr_cube)
+    return ((p_step_height + 0.25), curr_step)
 
 def set_perspective_camera():
     # Camera settings: retrieved by manually creating a camera and looking at the attribute values
